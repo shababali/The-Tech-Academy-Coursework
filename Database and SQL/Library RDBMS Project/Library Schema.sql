36ec0a15e8b4fce97a9c62270a7ac6597c1c493b
Shabab Ali <shabab.ali@mail.utoronto.ca>
2016-10-23 17:22:30 -0700

    Library RDBMS Construction Script
    A script for creating a Library RDBMS using Microsoft SQL server 2012.
    The script adapts the Tech Academy Schema in SQL_Library_Drill.pdf and
    makes a few semantic changes. The semantic changes along with primary
    and foregin key relationships are noted in coding comments.

---
+++ Database and SQL/Library RDBMS Project/Library Schema.sql
@@ -0,0 +1,130 @@
+/*--------------------------------------
+   SQL DRILL: LIBRARY DATABASE DESIGN
+           AND QUERY SOLTIONS
+---------------------------------------*/
+
+
+/*The library schema utilizes 3 KERNEL (REFERENCE) TABLES that are essential objects for a functioning 
+library relational database management system:
+
+1. LIBRARY_BRANCH (Repository for the books), 
+2. BOOK (The physical content exchanged), and
+3. BORROWER (patron taking books from the institution)  
+
+  The other Tables are attributes of the KERNELS: 
+   
+	BOOK - PUBLISHER, BOOK AUTHOR, BOOK_COPIES, BOOK_LOANS
+	LIBRARY_BRANCH - BOOK_COPIES, BOOK_LOANS
+	BORROWER - BOOK_LOANS 
+
+	Certain attributes values facilitate methods for linking data indexes, whith concomitant 
+	separation of data into a system of related data tables that are semantic. Some examples of these semantic 
+	features are:
+	- limiting data dipslay to not convolute a table eg separate publisher data from inventory data,
+	- accessing meaningful information directly from data table values eg copies of books in a particular branch, 
+	- security of sensitive values such as separating borrower name and address from a book loan. 
+	etc.
+*/
+
+
+  /* CREATING LIBRARY DATABASE 
+-------------------------------*/
+USE MASTER
+GO
+
+IF  EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'dbLibrarySA')
+DROP DATABASE [dbLibrarySA]
+GO
+
+CREATE DATABASE [dbLibrarySA]
+GO
+
+USE dbLibrarySA
+GO 
+
+
+  /* CREATING TABLES AND INDEXING PRIMARY-FOREIGN KEY RELATIONSHIPS
+--------------------------------------------------------------------*/
+
+
+CREATE TABLE BOOK --Refference Table
+(
+BookID int NOT NULL PRIMARY KEY,
+Title varchar (50) NOT NULL,
+PublisherID int NOT NULL --Semantic Change to SQL Drill Library Schema :- Add foreign key PublisherID that constrains relationship to Table PUBLISHER 
+)
+GO
+
+
+CREATE TABLE LIBRARY_BRANCH --Refference Table
+(
+BranchID int NOT NULL PRIMARY KEY,
+BranchName varchar (50) NOT NULL,
+[Address] varchar (100) NOT NULL
+)
+GO
+
+
+CREATE TABLE BORROWER --Refference Table
+(
+CardNo int NOT NULL PRIMARY KEY,
+BLastName varchar (50) NOT NULL,
+BFirstName varchar (50) NOT NULL,--Semantic Change to SQL Drill Library Schema :- Separate Borrower Last Name and First Name
+[Address] varchar (100) NOT NULL,
+Phone varchar (25) NOT NULL
+)
+GO
+
+
+			CREATE TABLE PUBLISHER 
+			(
+			PublisherID int NOT NULL PRIMARY KEY, --Inclusion of a semantic primary key PublisherID
+			PublisherName varchar (50) NOT NULL,  
+			[Address] varchar (100) NOT NULL,
+			Phone varchar (25) NOT NULL
+			)
+			GO
+
+	ALTER TABLE BOOK 
+	ADD CONSTRAINT fk_PerBOOK
+	FOREIGN KEY (PublisherID) REFERENCES PUBLISHER(PublisherID) --Relates Table PUBLISHER (JOIN ON PublisherID) back to Table BOOK
+
+
+		CREATE TABLE BOOK_AUTHORS 
+		(
+		BookID int NOT NULL PRIMARY KEY,   
+		AuthorLastName varchar (50) NOT NULL, --Semantic Change to SQL Drill Library Schema :- Separate Author Last Name and First Name
+		AuthorFirstName varchar (50) NOT NULL,
+		CONSTRAINT fk_PerBOOK_AUTHORS 
+		FOREIGN KEY (BookID) REFERENCES BOOK(BookID) --Relates Table BOOK_AUTHORS (JOIN ON BookID) back to Table BOOK
+		)
+		GO
+
+
+		CREATE TABLE BOOK_COPIES 
+		(						 
+		BookID int NOT NULL PRIMARY KEY, 
+		BranchID int NOT NULL,			 
+		No_of_Copies int NOT NULL,
+		CONSTRAINT fk_PerBOOK_COPIES		
+	    FOREIGN KEY (BookID) REFERENCES BOOK(BookID), --Relates Table BOOK_COPIES (JOIN ON BookID) back to Table BOOK
+		FOREIGN KEY (BranchID) REFERENCES LIBRARY_BRANCH(BranchID) --Relates Table BOOK_COPIES (JOIN ON BranchID) back to Table LIBRARY_BRANCH
+		)
+		GO
+
+		
+		CREATE TABLE BOOK_LOANS 
+		(						
+		BookID int PRIMARY KEY NOT NULL, 
+		BranchID int NOT NULL,			 
+		CardNo int NOT NULL,
+		DateOut date NOT NULL, --date data type format is YYYY-MM-DD.
+		DueDate date NOT NULL,
+		CONSTRAINT fk_PerBOOK_LOANS	  
+		FOREIGN KEY (BookID) REFERENCES BOOK(BookID), --Relates Table BOOK_LOANS (JOIN ON BookID) back to Table BOOK
+		FOREIGN KEY (BranchID) REFERENCES LIBRARY_BRANCH(BranchID), --Relates Table BOOK_LOANS (JOIN ON BranchID) back to Table LIBRARY_BRANCH
+		FOREIGN KEY (CardNo) REFERENCES BORROWER(CardNo) --Relates Table BOOK_LOANS (JOIN ON CardNo) back to Table BORROWER
+		)
+		GO
+
+
